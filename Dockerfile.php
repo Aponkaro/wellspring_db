0cd745982fe7c57f3d0e4c731ee90a0820462c10
Aponkaro <aponkaroedmond4@gmail.com>
2026-09-09 17:03:01 +0000

    Update Dockerfile.php

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,15 +1,3 @@
-FROM php:8.2-apache
-
-# Install PDO MySQL extensions
-RUN docker-php-ext-install pdo pdo_mysql && docker-php-ext-enable pdo_mysql
-
-# Enable Apache Mod Rewrite
-RUN a2enmod rewrite
-
-# Copy system code to Apache web root
-COPY . /var/www/html/
-
-# Set working directory permissions
-RUN chown -R www-data:www-data /var/www/html
-
-EXPOSE 80
+git rm Dockerfile
+git commit -m "Remove Dockerfile for native PHP deploy"
+git push origin main
