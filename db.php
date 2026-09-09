b1db05d5a3cc6c3c658ed00af70d5e692ca5b5fc
Aponkaro <aponkaroedmond4@gmail.com>
2026-09-09 18:53:52 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$host = getenv('DB_HOST') ?: '127.0.0.1';
-$db   = getenv('DB_NAME') ?: 'wellspring_db';
-$user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: '';
+$host = getenv('DB_HOST') ?: 'dpg-dagoap2d0e5s73dbp6tg-a';
+$db   = getenv('DB_NAME') ?: 'wellspring_db_8435';
+$user = getenv('DB_USER') ?: 'water_user';
+$pass = getenv('DB_PASS') ?: 'N6mRudDMV0G8YbtzIeZ1CK1IV195Zw2W';
 $port = getenv('DB_PORT') ?: '5432';
 
-// Explicitly specifying host and port forces TCP/IP connection instead of Unix socket
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
+// pgsql DSN format
+$dsn = "pgsql:host=$host;port=$port;dbname=$db";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
