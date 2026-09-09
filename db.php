b506426e34e58c79ddd9ead937e8399c16636371
Aponkaro <aponkaroedmond4@gmail.com>
2026-09-09 18:26:05 +0000

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ $host = getenv('DB_HOST') ?: '127.0.0.1';
 $db   = getenv('DB_NAME') ?: 'wellspring_db';
 $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASS') ?: '';
-$port = getenv('DB_PORT') ?: '3306';
+$port = getenv('DB_PORT') ?: '5432';
 
 // Explicitly specifying host and port forces TCP/IP connection instead of Unix socket
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
